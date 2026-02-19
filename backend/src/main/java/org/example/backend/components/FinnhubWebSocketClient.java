0530fa9b33f2f7bd49edf6b119384bb57a70acc1
Jonas Tholl <jonas@tholls.de>
2026-02-19 14:08:40 +0100

    added WebSocket class

---
+++ backend/src/main/java/org/example/backend/components/FinnhubWebSocketClient.java
@@ -0,0 +1,104 @@
+package org.example.backend.components;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.example.backend.models.FinnhubResponse;
+import org.example.backend.models.FinnhubResponseData;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
+
+@Component
+@RequiredArgsConstructor
+public class FinnhubWebSocketClient implements WebSocket.Listener {
+
+    private WebSocket webSocket;
+    private final LivePriceStore livePriceStore;
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final List<String> symbolsToSubscribe = new CopyOnWriteArrayList<>();
+
+    @PostConstruct
+    public void connect() {
+        HttpClient client = HttpClient.newHttpClient();
+
+        client.newWebSocketBuilder()
+                .buildAsync(
+                        URI.create("wss://ws.finnhub.io?token=" + System.getenv("FINNHUB_API_TOKEN")),
+                        this
+                )
+                .thenAccept(ws -> {
+                    this.webSocket = ws;
+                });
+    }
+
+    @Override
+    public CompletionStage<?> onText(WebSocket webSocket,
+                                     CharSequence data,
+                                     boolean last) {
+        try {
+            FinnhubResponse finnhubResponse = mapper.readValue(data.toString(), FinnhubResponse.class);
+            for (FinnhubResponseData finnhubResponseData : finnhubResponse.data()) {
+                livePriceStore.updatePrice(finnhubResponseData.s(), finnhubResponseData.p());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        webSocket.request(1);
+        return null;
+    }
+
+    public void addSymbol(String symbol) {
+        symbolsToSubscribe.add(symbol);
+
+        if (webSocket != null) {
+            sendSubscribe(symbol);
+        }
+    }
+
+    private void sendSubscribe(String symbol) {
+        String message = String.format(
+                "{\"type\":\"subscribe\",\"symbol\":\"%s\"}",
+                symbol
+        );
+        webSocket.sendText(message, true);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket) {
+        System.out.println("Connected!");
+        this.webSocket = webSocket;
+
+        for (String symbol : symbolsToSubscribe) {
+            sendSubscribe(symbol);
+        }
+
+        webSocket.request(1);
+    }
+
+    @Override
+    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+        System.out.println("Connection closed: " + reason + " (" + statusCode + ")");
+        reconnect();
+        return null;
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Throwable error) {
+        System.err.println("WebSocket error: " + error.getMessage());
+        reconnect();
+    }
+
+    private void reconnect() {
+        CompletableFuture.delayedExecutor(5, TimeUnit.SECONDS).execute(this::connect);
+    }
+
+
+}
